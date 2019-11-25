45ad60df775403c2a6a437358f1d2dbbe9e49aaa
Twin Edo <venganzdektor26@gmail.com>
2019-11-25 21:00:41 +0700

    menambah cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 const bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors');
 const db = require('./queries');
+app.options('*', cors());
 
 app.use(bodyParser.json())
 app.use(
@@ -10,12 +12,19 @@ app.use(
   })
 )
 
-app.get('/', function (req, res) {
+app.get('/', cors(), function (req, res) {
   res.send('Hello World!');
+  res.json({
+    msg: 'This is CORS-enabled for localhost:3000 domain'
+  })
 // response.json({ info: 'Node.js, Express, and Postgres API' })
 });
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
-app.get('/kendaraan', db.allKendaraan);
+app.get('/kendaraan', cors(), db.allKendaraan);
+
 app.get('/availableVehicle', db.getAvailableVehicle);
 app.get('/pengemudi', db.allPengemudi);
 app.get('/availableDriver', db.getAvailableDriver);
@@ -29,7 +38,9 @@ app.get('/activeVehicle', db.getActiveVehicle);
 app.get('/manifest/:id_penghasil&:plat', db.getManifestbyPenghasil);
 app.get('/manifest/:plat', db.getManifestbyTruck);
 app.get('/kendaraan/:plat', db.detailKendaraan);
-app.post('/kendaraan', db.createKendaraan);
+
+app.options('/kendaraan', cors());
+app.post('/kendaraan', cors(), db.createKendaraan);
 app.post('/pengemudi', db.createPengemudi);
 app.post('/penghasil', db.createPenghasil);
 app.post('/ijin', db.createIjin);
--- queries.js
+++ queries.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'db_hazwaste',
+  database: 'hazwate',
   password: 'admin',
   port: 5432,
 })
